e17079d3e8b5489336faaca6efce48fde2f1196e
Nejarul Islam <79055019+nejaruli4460@users.noreply.github.com>
2023-12-06 18:41:38 +0530

    intital backend

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$db="research scholar";
+$con=mysqli_connect($host,$user,$pass,$db);
+?>
--- result.php
+++ result.php
@@ -1,10 +1,17 @@
+<?php
+include("connection.php");
+if(isset($_GET["submit"])){
+    $sql="SELECT * FROM article_listing_google_scholar_data_mining";
+    $res=mysqli_query($con,$sql);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search Result</title>
     <link rel="stylesheet" href="style.css">
-    <title>Search Engine For Scholar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
@@ -14,24 +21,26 @@
             <img src="/Assets/University_of_Calcutta_logo.svg.png" alt="Logo" width="30" height="24">
           </a>
           <ul class = "signupLoginList">
-            <li><a class="nav-link" href="logIn.html">Login</a></li>
+            <li><a class="nav-link" href="#">Login</a></li>
           </ul>
         </div>
     </nav>
+  
 
-    <form class="search_bar" action="result.php" method="get">
-        <input type="text" name="search_key" id=""class="input_section">
-        <input type="submit" value="Search" name="submit" id="" class="search_button">
-    </form>
-
-    <footer>
-      <hr>
-      <div class="footer">
-        <a href="#">Home</a>
-        <a href="#">About Us</a>
-      </div>
-    </footer>
-
+<?php
+    while($result=mysqli_fetch_array($res)){
+    ?>
+    <div class="card">
+  <h5 class="card-header"><?php echo $result['Title']; ?></h5>
+  <div class="card-body">
+    <h5 class="card-title"><?php echo $result['Author']; ?></h5>
+    <p class="card-text"><?php echo $result['Description']; ?></p>
+    <a href=<?php  echo $result['Article_Link']; ?> class="btn btn-primary">Visit For More Details</a>
+  </div>
+</div>
+    <?php
+    }
+    ?>
     <script src="script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
