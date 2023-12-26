93cd1da6a699f336b0ce6d83f51337c282db93a6
b-nimai <nimaibarman4978@gmail.com>
2023-12-26 16:12:35 +0530

    Second Last final

--- result.php
+++ result.php
@@ -14,14 +14,16 @@ if(isset($_GET["submit"])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search Result</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="./Assets/logo.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
     <nav class="navbar bg-primary">
         <div class="container">
-          <a class="navbar-brand" href="#">
-            <img src="/Assets/University_of_Calcutta_logo.svg.png" alt="Logo" width="30" height="24">
-          </a>
+            <a class="navbar-brand text-white" href="index.html">
+                <img src="./Assets/logo.png" alt="Logo" width="55" height="30">
+                NextScholar
+            </a>
           <ul class = "signupLoginList">
             <li><a class="nav-link" href="#">Login</a></li>
           </ul>
